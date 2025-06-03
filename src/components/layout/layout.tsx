f1f2fe4b2315c55a9a1122b91ecad481d7d06ddf
Narendra Singh Chouhan <115109945+monu808@users.noreply.github.com>
2025-06-03 21:17:36 +0530

    Fix Navbar import in layout component

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Navbar } from "./navbar";
+import Navbar from "./navbar";
 import { Footer } from "./footer";
 
 export function Layout() {
